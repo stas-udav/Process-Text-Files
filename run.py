8eadff3362456423f262610fe6dc916b93c54d2d
stan.hordon <stan.hordon@gmail.com>
2023-10-31 17:17:03 -0700

    add json dump

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 
 import os
 import requests
+import json
+
 
 feedback_dir =  "D:\\QA\python\\feedback" # varible with path
 feedback_list = os.listdir(feedback_dir) # creating list of files with feedbacks
@@ -15,9 +17,13 @@ for files in feedback_list : # Create dict w feedback content
             content_list.append(line)
         feedback_dict ={"title": content_list[0], "name": content_list[1], "date": content_list[2], "feedback": content_list[3]}
         feedback_all.append(feedback_dict) #add dict to feedback dict list
-print (feedback_all)
+#print (feedback_all)
+json_data = json.dumps(feedback_all)
 #response = request.post(http://<corpweb-external-IP>/feedback, json=feedback_all)
-if  200<= request.post(http://<corpweb-external-IP>/feedback, json=feedback_all) <= 299:
+#if 200<= request.post(http://<corpweb-external-IP>/feedback, json=feedback_all) <= 299:
+#response = requests.post("http://35.226.11.38/feedback", json=feedback_all)
+response = requests.post("http://35.226.11.38/feedback", json=json_data, headers={"Content-Type": "application/json"})
+if response.status_code == 201:
     print("success", response.status_code)
 else :
     print("Error", response.status_code)
