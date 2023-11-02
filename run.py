a997647c651aa2a9801bb82bb2152a03f60694a7
stan.hordon <stan.hordon@gmail.com>
2023-11-02 11:10:18 -0700

    optimize script, change sending method

--- run.py
+++ run.py
@@ -1,29 +1,17 @@
-#!/usr/bin/env python3
-
+#! /usr/bin/env python3
 import os
 import requests
-import json
-
-
-feedback_dir =  "D:\\QA\python\\feedback" # varible with path
-feedback_list = os.listdir(feedback_dir) # creating list of files with feedbacks
-feedback_all = [] # list od all dicteonary deedback
-
-for files in feedback_list : # Create dict w feedback content 
-    file_open =  os.path.join(feedback_dir, files)
-    with open(file_open, "r") as feedback :
-        content_list = [] #read line by line  creating value for dictionary
-        for line in feedback:
-            content_list.append(line)
-        feedback_dict ={"title": content_list[0], "name": content_list[1], "date": content_list[2], "feedback": content_list[3]}
-        feedback_all.append(feedback_dict) #add dict to feedback dict list
-#print (feedback_all)
-json_data = json.dumps(feedback_all)
-#response = request.post(http://<corpweb-external-IP>/feedback, json=feedback_all)
-#if 200<= request.post(http://<corpweb-external-IP>/feedback, json=feedback_all) <= 299:
-#response = requests.post("http://35.226.11.38/feedback", json=feedback_all)
-response = requests.post("http://35.226.11.38/feedback", json=json_data, headers={"Content-Type": "application/json"})
-if response.status_code == 201:
-    print("success", response.status_code)
-else :
-    print("Error", response.status_code)
+BASEPATH = '/data/feedback/' # varible with path
+folder = os.listdir(BASEPATH)
+list = []
+for file in folder:
+   with open(BASEPATH + file, 'r') as f:
+       list.append({"title":f.readline().rstrip("\n"), # простой способ чтения данных из файлов с отзывами
+           "name":f.readline().rstrip("\n"),
+           "date":f.readline().rstrip("\n"),
+           "feedback":f.read().rstrip("\n")})
+for item in list:
+   resp = requests.post('http://[corp-web-external-IP]/feedback/', json=item) #отправляет данные на сервер по одной записи за раз
+   if resp.status_code != 201:
+       raise Exception('POST error status={}'.format(resp.status_code))
+   print('Created feedback ID: {}'.format(resp.json()["id"]))
